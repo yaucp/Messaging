a0327f85584640558f3666b901add5c810de4829
Thomas Yau <yaucp@connect.hku.hk>
2022-02-22 19:14:18 +0200

    add ip

--- msgclient.py
+++ msgclient.py
@@ -2,25 +2,68 @@ import socket
 import threading
 import json
 from datetime import datetime
+import argparse
 
 
 # JSON FORMAT:
 # From Host: NEWMSG SENDER TIMESTAMP | ARGS: MSG
+# From Host: CACHEDMSG | ARGS: [ith sender timestamp msg]
 # From Client: SENDMSG RECEIVER TIMESTAMP | ARGS: MSG (cmd ver: send thomas hello)
 
-def write(groups_lock):
-    global groups
+def write():
     while True:
+        if not receive_thread.is_alive():
+            exit(0)
         msg_input = input()
-        cmd, rest = msg_input.split(' ', 1)
-        if cmd[0] == "send":
-            sender, msg = rest.split(' ', 1)
-            data = json.dumps({'cmd': ['SENDMSG', sender, datetime.now()], 'args': msg})
+        if msg_input == "quit":
+            data = json.dumps({'cmd': ['QUIT']})
             client.send(data.encode())
+            exit(0)
+        cmd, rest = msg_input.split(' ', 1)
+        if cmd == 'send':
+            receiver, msg = rest.split(' ', 1)
+            if receiver == username:
+                print("You can't send message to yourself!")
+                continue
+            data = json.dumps(
+                {'cmd': ['SENDMSG', receiver, str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))], 'args': msg})
+            client.send(data.encode())
+        elif cmd == 'create':
+            if len(rest.split()) < 2:
+                print("Error: missing arguments.")
+                continue
+            group_name, members = rest.split(' ', 1)
+            members = members.split()
+            if username in members:
+                members.remove(username)
+                if len(members) == 0:
+                    print("Please retype as you cannot create a group with only yourself.")
+                    continue
+            data = json.dumps({'cmd': ['CREATEGROUP', group_name, username], 'args': members})
+            client.send(data.encode())
+        elif cmd == 'add':
+            if len(rest.split()) < 2:
+                print("Error: missing arguments.")
+                continue
+            group_name, new_members = rest.split(' ', 1)
+            new_members = new_members.split()
+            if username in new_members:
+                new_members.remove(username)
+            data = json.dumps({'cmd': ['GROUPMAN', 'ADD', group_name], 'args': new_members})
+            client.send(data.encode())
+        elif cmd == 'remove':
+            if len(rest.split()) < 2:
+                print("Error: missing arguments.")
+                continue
+            group_name, remove_members = rest.split(' ', 1)
+            remove_members = remove_members.split()
+            data = json.dumps({'cmd': ['GROUPMAN', 'REMOVE', group_name], 'args': remove_members})
+            client.send(data.encode())
+        elif cmd == 'remove':
+            pass
 
 
-def receive(groups_lock):
-    global groups
+def receive():
     data = client.recv(2048).decode()
     init_cmd = json.loads(data)
     if init_cmd['cmd'][0] == 'NICK':
@@ -34,44 +77,90 @@ def receive(groups_lock):
     else:
         print("Error in establishing username with host. Closed connection.")
         client.close()
-        return
+        exit
     while True:
         try:
             data = client.recv(2048).decode()
+            print(data)
             data_loaded = json.loads(data)
             cmd = data_loaded['cmd']
-            if cmd[0] == 'NEWMSG':
+            if cmd[0] == 'NEWUSER':
+                print(f"System: {data_loaded['args']} has joined the chat!")
+            elif cmd[0] == 'NEWMSG':
                 sender = cmd[1]
-                timestamp = cmd[2].strftime("%Y-%m-%d %H:%M:%S")
+                timestamp = cmd[2]
                 print(f"{str(sender)} [{timestamp}]: {data_loaded['args']}")
             elif cmd[0] == 'ERROR':
-                print(data_loaded['args'])
+                print(f"ERROR: {data_loaded['args']}")
             elif cmd[0] == 'OFFLINEMSG':
                 print(
-                    f"\n\***** ALERT: {cmd[1]} is offline right now. The message will be sent again once it is online. *****/\n")
-        except:
+                    f"\n\***** ALERT: {cmd[1]} is offline right now. His/her last online time is {cmd[2]}. The message will be sent again once he/she is online. *****/\n")
+            elif cmd[0] == 'CACHEDMSG':
+                # CACHEDMSG i sender timestamp msg
+                cached_messages = data_loaded['args']
+                print("\nYou have the following buffered message when you're offline:\n")
+                for cached_message in cached_messages:
+                    sender = cached_message[1]
+                    timestamp = cached_message[2]
+                    print(f"|{cached_message[0]}th message| {str(sender)} [{timestamp}]: {cached_message[3]}")
+            elif cmd[0] == 'CACHEDGPMSG':
+                cached_messages = data_loaded['args']
+                print("\nYou have the following buffered group message when you're offline:\n")
+                for cached_message in cached_messages:
+                    gp_name = cached_message[1]
+                    sender = cached_message[2]
+                    timestamp = cached_message[3]
+                    print(
+                        f"|{cached_message[0]}th message| |{gp_name}| {str(sender)} [{timestamp}]: {cached_message[3]}")
+            elif cmd[0] == 'NEWGPMSG':
+                gp_name = cmd[1]
+                sender = cmd[2]
+                timestamp = cmd[3]
+                print(f"|{gp_name}| {sender} [{timestamp}]: {data_loaded['args']}")
+            elif cmd[0] == 'OFFLINEGPMSG':
+                print(
+                    f"\n\***** ALERT: {cmd[2]} in {cmd[1]} is offline right now. His/her last online time is {cmd[3]}. The message will be sent again once he/she is online. *****/\n")
+            elif cmd[0] == 'QUIT':
+                client.close()
+                exit(0)
+            elif cmd[0] == 'BROADCAST':
+                print(data_loaded['args'])
+        except Exception as msg:
             print("Error occured. Closing connection now....")
+            print(msg)
+            print(data_loaded)
             client.close()
-            return
+            exit(0)
 
 
-client = socket.socket()
-groups_lock = threading.Lock()
-groups = {}
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--ipv", help="IP version")
+args = parser.parse_args()
+
+port = 0
+if args.ipv == "4":
+    client = socket.socket()
+    port = 12345
+elif args.ipv == "6":
+    client = socket.socket(family=socket.AF_INET6)
+    port = 12346
+else:
+    print("Wrong IP Version!")
+    exit(0)
+
 username = input("Enter your username (it must have no spaces!): ")
 
 while True:
     host = input("Enter host IP address: ")
-    port = input("Enter host port: ")
 
     try:
         client.connect((host, port))
         break
-    except:
-        print("Failed to connect. Please retry.\n")
+    except socket.error as msg:
+        print(f"Error: {msg}\n")
 
-receive_thread = threading.Thread(target=receive, args=(groups_lock,))
+receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
-write_thread = threading.Thread(target=write, args=(groups_lock,))
+write_thread = threading.Thread(target=write)
 write_thread.start()
--- msghost.py
+++ msghost.py
@@ -5,82 +5,251 @@ from datetime import datetime
 
 
 def broadcast(msg):
-    for client in clients.items():
-        client.send(msg)
+    clients_lock.acquire()
+    for broad_client in clients.values():
+        broad_client.send(msg.encode())
+    clients_lock.release()
 
 
 def handle(client, name, groups_lock, clients_lock):
     global groups
     while True:
         try:
-            data = client.recv(2048).decode()
-            data_loaded = json.loads(data)
+            data = client.recv(2048)
+            clients_lastonline[name] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            data_loaded = json.loads(data.decode())
             log(name, f"Recevied following data: {data_loaded}")
             cmd = data_loaded['cmd']
             if cmd[0] == 'SENDMSG':
                 receiver = cmd[1]
                 clients_lock.acquire()
-                if receiver not in clients:
+                groups_lock.acquire()
+                if receiver in groups and name in groups[receiver]['members']:
+                    for member in groups[receiver]['members']:
+                        if member == name:
+                            continue
+                        try:
+                            data = json.dumps(
+                                {'cmd': ['NEWGPMSG', receiver, name, cmd[2]], 'args': data_loaded['args']})
+                            clients[member].send(data.encode())
+                        except:
+                            log(name, f"{receiver} unable to receive message. Cached.")
+                            data = json.dumps({'cmd': ['OFFLINEGPMSG', receiver, member, clients_lastonline[member]],
+                                               'args': data_loaded['args']})
+                            client.send(data.encode())
+                            data_loaded['cmd'].insert(2, name)
+                            gp_cache.setdefault(member, []).append(data_loaded)
+                elif receiver not in clients and receiver not in clients_lastonline:
                     log(name, f"Tried to send message to {receiver} but it doesn't exist.")
-                    data = json.dumps({'cmd':["ERROR"], 'args':f"Recevier {receiver} doesn't exist."})
+                    data = json.dumps({'cmd': ["ERROR"], 'args': f"Recevier {receiver} doesn't exist."})
                     client.send(data.encode())
                 else:
                     try:
-                        data = json.dumps({'cmd':['NEWMSG',name, cmd[1]], 'args': data_loaded['args']})
+                        data = json.dumps({'cmd': ['NEWMSG', name, cmd[2]], 'args': data_loaded['args']})
                         clients[receiver].send(data.encode())
                     except:
                         log(name, f"{receiver} unable to receive message. Cached.")
-                        data = json.dumps({'cmd':['OFFLINEMSG', receiver], 'args': data_loaded['args']})
+                        data = json.dumps({'cmd': ['OFFLINEMSG', receiver, clients_lastonline[receiver]],
+                                           'args': data_loaded['args']})
                         client.send(data.encode())
+                        data_loaded['cmd'][1] = name
+                        cache.setdefault(receiver, []).append(data_loaded)
+                groups_lock.release()
                 clients_lock.release()
+            elif cmd[0] == 'CREATEGROUP':
+                group_name = cmd[1]
+                admin = cmd[2]
+                members = data_loaded['args']
+                groups_lock.acquire()
+                if group_name in groups:
+                    log(name, f"Group '{group_name}' already exists.")
+                    data = json.dumps({'cmd': ["ERROR"],
+                                       'args': f"Group '{group_name}' already exists. Please pick a new group name."})
+                    client.send(data.encode())
+                else:
+                    all_exist = True
+                    clients_lock.acquire()
+                    for member in members:
+                        if member not in clients and member not in clients_lastonline:
+                            log(name, f"Tried to add {receiver} user to group {group_name} but the user doesn't exist.")
+                            data = json.dumps(
+                                {'cmd': ["ERROR"], 'args': f"User {receiver} doesn't exist. Please retry"})
+                            client.send(data.encode())
+                            all_exist = False
 
-        except:
+                    if all_exist:
+                        members.append(admin)
+                        groups[group_name] = {'admin': admin, 'members': members}
+                        for member in members:
+                            if member != name:
+                                data = json.dumps(
+                                    {'cmd': ["BROADCAST"],
+                                     'args': f"You have been added to group '{group_name}' with members of {members}."})
+                                clients[member].send(data.encode())
+                            else:
+                                data = json.dumps(
+                                    {'cmd': ["BROADCAST"],
+                                     'args': f"You have created group '{group_name}' with members of {members}."})
+                                client.send(data.encode())
+                    clients_lock.release()
+                print(f"Current group list: {groups}")
+                groups_lock.release()
+            elif cmd[0] == 'GROUPMAN':
+                group_name = cmd[2]
+                groups_lock.acquire()
+                if group_name not in groups:
+                    data = json.dumps(
+                        {'cmd': ["ERROR"], 'args': f"'{group_name}' group doesn't exist."})
+                    client.send(data.encode())
+                elif groups[group_name]['admin'] == name:
+                    if cmd[1] == 'ADD':
+                        members_list = data_loaded['args']
+                        for member in members_list:
+                            if member not in groups[group_name]['members']:
+                                groups[group_name]['members'].append(member)
+                        clients_lock.acquire()
+                        for member in groups[group_name]['members']:
+                            data = json.dumps(
+                                {'cmd': ["BROADCAST"],
+                                 'args': f"Group '{group_name}' has been updated with new member list: {members}."})
+                            clients[member].send(data.encode())
+                        clients_lock.release()
+                    elif cmd[1] == 'REMOVE':
+                        members_list = data_loaded['args']
+                        for member in members_list:
+                            if member in groups[group_name]['members']:
+                                groups[group_name]['members'].remove(member)
+                                data = json.dumps(
+                                    {'cmd': ["BROADCAST"],
+                                     'args': f"You have been removed from group '{group_name}'."})
+                                clients[member].send(data.encode())
+                        clients_lock.acquire()
+                        for member in groups[group_name]['members']:
+                            data = json.dumps(
+                                {'cmd': ["BROADCAST"],
+                                 'args': f"Group '{group_name}' has been updated with new member list: {members}."})
+                            clients[member].send(data.encode())
+                        clients_lock.release()
+                else:
+                    data = json.dumps(
+                        {'cmd': ["ERROR"], 'args': f"You are not the admin of '{group_name}'."})
+                    client.send(data.encode())
+                groups_lock.release()
+            elif cmd[0] == 'QUIT':
+                clients_lock.acquire()
+                del clients[name]
+                clients_lock.release()
+                broadcast(json.dumps({'cmd': ['BROADCAST'], 'args': f"{name} has left the chat."}))
+                data = json.dumps({'cmd': ['QUIT']})
+                client.send(data.encode())
+                exit(0)
+            else:
+                data = json.dumps(
+                    {'cmd': ["ERROR"], 'args': "Invalid command."})
+                client.send(data.encode())
+
+
+        except Exception as msg:
+            # broadcast missing of client
+            clients_lock.acquire()
+            if name in clients:
+                del clients[name]
+            clients_lock.release()
+            broadcast(json.dumps({'cmd': ['ERROR'], 'args': f"{name}'s client occured an error."}))
             print("Error occured. Closing connection now....")
+            print(msg)
             client.close()
-            return
+            exit(0)
+
 
 def log(addr, msg):
     print(f"{str(addr)}: {msg}")
 
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12346
-print(f"IP: {str(socket.gethostbyname(host))}")
+def setup(socket):
+    global clients
+    while True:
+        client, addr = socket.accept()
+        print(f"Connected with {str(addr)}")
 
-s.bind((host, port))
-s.listen()
+        cmd = json.dumps({'cmd': ['NICK'], 'args': ''})
+        client.send(cmd.encode())
+
+        data = client.recv(2048).decode()
+        name = json.loads(data)
+        if name['cmd'][0] == 'NAME':
+            clients_lock.acquire()
+            clients[name['args']] = client
+            clients_lastonline[name['args']] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            clients_lock.release()
+            log(addr, f"Setup its nickname as {name['args']}")
+            broadcast(json.dumps({'cmd': ['NEWUSER'], 'args': name['args']}))
+
+            if name['args'] in cache and len(cache[name['args']]) > 0:
+                log(name['args'], "Found cached message. Re-sending right now....")
+                i = 1
+                cache_to_sent = []
+                for cached_data in cache[name['args']]:
+                    log(name['args'], f"Sorting {str(i)}th cached message...")
+                    cached_data_cmd = cached_data['cmd']
+                    cache_to_sent.append([i, cached_data_cmd[1], cached_data_cmd[2], cached_data['args']])
+                    i += 1
+                data = json.dumps({'cmd': ['CACHEDMSG'], 'args': cache_to_sent})
+                log(name['args'], f"Sending following cached data: {data}")
+                client.send(data.encode())
+                cache[name['args']] = []
+
+            if name['args'] in gp_cache and len(gp_cache[name['args']]) > 0:
+                log(name['args'], "Found cached group message. Re-sending right now....")
+                i = 1
+                cache_to_sent = []
+                for cached_data in gp_cache[name['args']]:
+                    log(name['args'], f"Sorting {str(i)}th cached group message...")
+                    cached_data_cmd = cached_data['cmd']
+                    cache_to_sent.append(
+                        [i, cached_data_cmd[1], cached_data_cmd[2], cached_data_cmd[3], cached_data['args']])
+                    i += 1
+                data = json.dumps({'cmd': ['CACHEDGPMSG'], 'args': cache_to_sent})
+                log(name['args'], f"Sending following cached group data: {data}")
+                client.send(data.encode())
+                gp_cache[name['args']] = []
+
+            # Start Handling Thread For Client
+            thread = threading.Thread(target=handle, args=(client, name['args'], groups_lock, clients_lock))
+            thread.start()
+        else:
+            print(f"Error establishing username with {str(addr)}.")
+            print(f"Terminated connection with {str(addr)}.")
+            client.close()
+            exit
+
+
+s4 = socket.socket()
+s6 = socket.socket(family=socket.AF_INET6)
+
+host4 = socket.gethostname()
+host6 = socket.gethostname()
+port = 12345
+print(f"IPv4: {str(socket.gethostbyname(host4))}:{port}")
+print(f"IPv6: {str(socket.getaddrinfo(host6, port + 1, family=socket.AF_INET6)[0][4])}")
+
+s4.bind((host4, port))
+s4.listen(5)
+
+s6.bind((host6, port + 1))
+s6.listen(5)
 
 groups = {}
 groups_lock = threading.Lock()
 clients_lock = threading.Lock()
 
 clients = {}
+clients_lastonline = {}
 cache = {}
+gp_cache = {}
 
-while True:
-    client, addr = s.accept()
-    print(f"Connected with {str(addr)}")
+thread = threading.Thread(target=setup, args=(s4,))
+thread.start()
 
-    cmd = json.dumps({'cmd': ['NICK'], 'args': ''})
-    client.send(cmd.encode())
-
-    data = client.recv(2048).decode()
-    name = json.loads(data)
-    if name['cmd'][0] == 'NAME':
-        clients_lock.acquire()
-        clients[name['args']] = client
-        clients_lock.release()
-        log(addr, f"Setup its nickname as {name['args']}")
-        if name['args'] in cache and len(name['args']) > 0:
-            #                 Resend all cached message
-            pass
-        broadcast(json.dumps({'cmd': ['NEWUSER'], 'args': name['args']}).encode())
-
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client, name['args'], groups_lock, clients_lock))
-        thread.start()
-    else:
-        print(f"Error establishing username with {str(addr)}.")
-        print(f"Terminated connection with {str(addr)}.")
-        client.close()
+thread = threading.Thread(target=setup, args=(s6,))
+thread.start()
